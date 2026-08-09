ab79a79f67ab69b081c0bb7101cf470010a1b40a
Djimrosngue <djimrosnguejustin@gmail.com>
2026-08-09 17:06:12 +0200

    changement de base de données sqlite avec postgresql

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 
     "portfolio",
 ]
@@ -54,14 +55,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "djim_portfolio",
+        "USER": "justin",
+        "PASSWORD": "justin",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
